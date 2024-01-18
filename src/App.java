6ec146c6492ba450d4d8de7132f88add24207de6
Penelope <Penelope@email.com>
2024-01-18 18:14:44 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args){
+    String nombre;
+    double monto;
+    double resultado;
+
+    nombre="Penelope";
+            monto=475.50;
+            resultado=monto * 1.15;
+            System.out.println("hola" + nombre + "el valor total es " + resultado);
+
+
+
+    }
+}
